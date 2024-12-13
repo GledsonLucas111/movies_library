1967d435fb33de394af1e19aa6fd0aa91839b681
gledsonlucas111 <gledsonlucas111@gmail.com>
2024-12-13 09:22:30 -0300

    menu bar

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,17 @@
 import MenuBar from "@/components/menu_bar";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex h-full">
-      <MenuBar/>
+    <div className="grid grid-cols-[5rem_minmax(5rem,_1fr)] h-full">
+      <MenuBar />
+      <div>
+        <img
+          alt=""
+          src="/images/CAPA-lupin.png"
+          className="w-full"
+        />
+      </div>
     </div>
   );
 }
--- src/components/menu_bar/index.tsx
+++ src/components/menu_bar/index.tsx
@@ -3,13 +3,38 @@ import { MdMovie, MdSearch } from "react-icons/md";
 import { LuMonitor } from "react-icons/lu";
 
 export default function MenuBar() {
+  const icons = [
+    {
+      id: 1,
+      icon: <MdSearch />,
+    },
+    {
+      id: 2,
+      icon: <GoHome />,
+    },
+    {
+      id: 3,
+      icon: <LuMonitor />,
+    },
+    {
+      id: 4,
+      icon: <MdMovie />,
+    },
+    {
+      id: 5,
+      icon: <GoPlus />,
+    },
+  ];
+
   return (
-    <div className="bg-black h-full w-28 text-white flex justify-center items-center flex-col gap-14 text-3xl">
-      <MdSearch />
-      <GoHome />
-      <LuMonitor />
-      <MdMovie />
-      <GoPlus />
+    <div className="bg-zinc-950 h-full w-20 text-white flex justify-center items-center flex-col gap-14">
+      {icons.map((icon) => {
+        return (
+          <button key={icon.id} className="text-3xl">
+            {icon.icon}
+          </button>
+        );
+      })}
     </div>
   );
 }
